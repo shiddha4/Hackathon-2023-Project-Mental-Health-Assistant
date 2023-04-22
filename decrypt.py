59dbed048785f6a04239d83fc98c324c178aa95b
Sudarshan Surendranathan <88521427+shiddha4@users.noreply.github.com>
2023-04-22 14:59:32 -0400

    Update decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -2,20 +2,23 @@ from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 import sys
 
 
-def decrypt(filename, key_path):
-    with open(key_path, 'rb') as filekey:
-        key = filekey.read()
+def decrypt1(filename, key_path):
+    try:
+        with open(key_path, 'rb') as filekey:
+            key = filekey.read()
 
-    # fernet = fnet(key)
-    with open(filename, 'rb') as file:
-        ciphertext = file.read()
+        # fernet = fnet(key)
+        with open(filename, 'rb') as file:
+            ciphertext = file.read()
 
-    # decrypt = fernet.decrypt(original)
-    plaintext = AESGCM(key).decrypt(ciphertext[:12], ciphertext[12:], b"")
+        # decrypt = fernet.decrypt(original)
+        plaintext = AESGCM(key).decrypt(ciphertext[:12], ciphertext[12:], b"")
 
-    with open(filename, 'wb') as encrypted_file:
-        encrypted_file.write(plaintext)
-
+        with open(filename, 'wb') as encrypted_file:
+            encrypted_file.write(plaintext)
+        return True
+    except:
+        return False
 
 if __name__ == "__main__":
-    decrypt(sys.argv[1], sys.argv[2])
+    decrypt1(sys.argv[1], sys.argv[2])
